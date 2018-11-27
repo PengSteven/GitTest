277f5f77e2c631a60b4ae59d2cb69b38f8242af5
PengSteven <13392871998@163.com>
2018-11-27 10:48:57 +0800

    增加打印，修改List中的元素

--- testsort/src/main/java/test/pcl/com/testsort/MainActivity.java
+++ testsort/src/main/java/test/pcl/com/testsort/MainActivity.java
@@ -27,10 +27,11 @@ public class MainActivity extends Activity {
         //创建3个Person对象，年龄分别是32、20、25，并将他们依次放入List中
         Person p1 = new Person("0001","zhangsan",32);
         Person p2 = new Person("0002","lisi",20);
-        Person p3 = new Person("0003","wangwu",25);
+        Person p3 = new Person("0003","wangwu",27);
         plist.add(p1);
         plist.add(p2);
         plist.add(p3);
+		Log.v("MainActivity","begin");
         System.out.println("排序前的结果："+plist);
         Collections.sort(plist, new Comparator<Person>(){
             /*
